edb3e3325f8016cfef10206260d82d41620f0002
Mike Wittenauer <mikewittenauerwebdevelopment@gmail.com>
2016-10-13 08:52:46 -0400

    Add user model and test, also added bcrypt gem for has secure password

--- Gemfile
+++ Gemfile
@@ -13,6 +13,7 @@ gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'puma'
+gem 'bcrypt'
 
 group :development, :test do
   gem 'rspec-rails', '~> 3.5'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161012145426) do
+ActiveRecord::Schema.define(version: 20161013122542) do
 
   create_table "campaigns", force: :cascade do |t|
     t.string   "title"
@@ -22,6 +22,14 @@ ActiveRecord::Schema.define(version: 20161012145426) do
   create_table "sides", force: :cascade do |t|
     t.string   "title"
     t.integer  "campaign_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "full_name"
+    t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
