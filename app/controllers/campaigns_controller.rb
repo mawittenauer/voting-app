3c41800c8f20d02073d8a4a5fb7ac1c3f5030d05
Mike Wittenauer <mikewittenauerwebdevelopment@gmail.com>
2016-10-24 10:31:14 -0400

    Add pagination gem and add to ui

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -2,7 +2,7 @@ class CampaignsController < ApplicationController
   before_action :require_user
   
   def index
-    @campaigns = Campaign.all
+    @campaigns = Campaign.paginate(:page => params[:page], :per_page => 10)
   end
   
   def show
