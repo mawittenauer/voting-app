27c2a11d6edbe61e563b77d2d32e2929db0335ff
Mike Wittenauer <mikewittenauerwebdevelopment@gmail.com>
2016-11-04 10:47:00 -0400

    Add user token and digest methods for use in activation token

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  attr_accessor :activation_token
+  before_create :create_activation_digest
   has_secure_password
   validates_presence_of :full_name, :email
   has_many :votes
@@ -9,5 +11,22 @@ class User < ActiveRecord::Base
   
   def can_vote?(campaign)
     !votes.map{ |vote| vote.campaign }.include?(campaign)
+  end
+  
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+  
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+  
+  private
+  
+  def create_activation_digest
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest(activation_token)
   end
 end
