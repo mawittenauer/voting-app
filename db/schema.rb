7124a4cf3ab0b2ba2310485616fb875fd4a0f364
Mike Wittenauer <mikewittenauerwebdevelopment@gmail.com>
2016-11-04 10:38:55 -0400

    Add activation methods to user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161025132120) do
+ActiveRecord::Schema.define(version: 20161104143704) do
 
   create_table "campaigns", force: :cascade do |t|
     t.string   "title"
@@ -33,6 +33,9 @@ ActiveRecord::Schema.define(version: 20161025132120) do
     t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "activation_digest"
+    t.boolean  "activated"
+    t.datetime "activated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
